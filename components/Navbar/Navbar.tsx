d376e157876a1ac0e40f5f37cd9f5fecc8c3cfe6
Sukanta Das <arnobh84@gmail.com>
2023-09-04 21:33:09 +0600

    mobile sidebar added

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { MobileSidebar } from "../MobileSidebar/MobileSidebar";
 
-const Navbar = () => {
-  return <div></div>;
+// import { getApiLimitCount } from "@/lib/api-limit";
+// import { checkSubscription } from "@/lib/subscription";
+// import MobileSidebar from "../MobileSidebar/MobileSidebar";
+
+const Navbar = async () => {
+  //   const apiLimitCount = await getApiLimitCount();
+  //   const isPro = await checkSubscription();
+  const apiLimitCount = 5;
+  const isPro = false;
+
+  return (
+    <div className="flex items-center p-4">
+      <MobileSidebar
+        isPro={isPro}
+        apiLimitCount={apiLimitCount}
+      />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
