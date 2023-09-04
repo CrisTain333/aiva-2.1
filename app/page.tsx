743f71c918c441eee0eb47fb3bb8fe3ad6b87382
Sukanta Das <arnobh84@gmail.com>
2023-09-04 15:35:07 +0600

    channge

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <p className="text-red-500">Main</p>;
+  return (
+    <p className="text-red-500">
+      <Button variant="outline">Button</Button>
+    </p>
+  );
 }
