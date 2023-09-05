e412cab5c4b1404ee2e053f7ae14c5678e39fd0d
Sukanta Das <arnobh84@gmail.com>
2023-09-05 19:04:07 +0600

    Landing Navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,19 @@
+import { LandingNavbar } from "@/components/LandingNav/LandingNav";
+
+import { Montserrat } from "next/font/google";
 import React from "react";
 
+const font = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
 const LadingPage = () => {
-  return <div>Lading Page (Unprotected)</div>;
+  return (
+    <div>
+      {/* Nav Section */}
+      <LandingNavbar />
+    </div>
+  );
 };
 
 export default LadingPage;
